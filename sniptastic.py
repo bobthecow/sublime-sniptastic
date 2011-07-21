a2ef215e8d59c99627d74dc5cb91baf0e9037603
Ryan Hileman <lunixbochs@gmail.com>
2011-07-21 00:33:17 -0600

    added plugin

---
+++ sniptastic.py
@@ -0,0 +1,25 @@
+import sublime_plugin
+
+snippets = {
+	'foo':{'code':'if ($1) {$2; $3}', 'scope':'source'},
+	'bar':{'code':'class $1 extends $2 {\n\t$3\n}', 'scope':'source'}
+}
+
+class Sniptastic(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		view.run_command('single_selection')
+		names = self.view.scope_name(self.view.sel()[0].b)
+		scopes = []
+		for name in names.split(' '):
+			scope = []
+			for section in name.split('.'):
+				scope.append(section)
+				scopes.append('.'.join(scope))
+
+		candidates = [snippet for snippet in snippets if snippets[snippet]['scope'] in scopes]
+		def callback(idx):
+			snippet = candidates[idx]
+			self.view.run_command('insert_snippet', {'contents':snippets[snippet]['code']})
+
+		view.window().show_quick_panel(candidates, callback)
